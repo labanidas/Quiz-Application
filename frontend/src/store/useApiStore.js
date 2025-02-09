fc652978a3a19ffe79a85e890e90d8c2c445b62a
labanidas <labanidas2001@gmail.com>
2025-02-09 15:19:28 +0530

    toast

--- frontend/src/store/useApiStore.js
+++ frontend/src/store/useApiStore.js
@@ -1,28 +1,31 @@
 // src/store/useApiStore.js
 import { defineStore } from 'pinia';
 import { ref } from 'vue';
-import { axiosInstance } from '../lib/axios'; // Import the axiosInstance
+import { axiosInstance } from '../lib/axios'; 
+import { useToast } from 'vue-toast-notification';  
 
 export const useApiStore = defineStore('apiStore', () => {
   const message = ref(''); 
+  const toast = useToast(); 
 
   // API call method using Axios
   const fetchMessage = async () => {
     try {
       const response = await axiosInstance.get('/'); 
       message.value = response.data.message;
-    } catch (error) {
-      if (error.response) {
-        console.error('Server Error:', error.response.data);
-      } else if (error.request) {
-        console.error('Network Error:', error.request);
-      } else {
-        console.error('Error:', error.message);
-      }
-    }finally{
 
+
+      toast.success('Backend connected successfully!', {
+        position: 'top-right', 
+        timeout: 3000, 
+      });
+    } catch (error) {
+      toast.error(error.message || 'Something went wrong!', {
+        position: 'top-right',
+        timeout: 3000,
+      });
     }
   };
 
-  return { message, fetchMessage }; // Return the state and method
+  return { message, fetchMessage }; // Return the reactive state and method
 });
