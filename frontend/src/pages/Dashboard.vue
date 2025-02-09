e35bad43e4bacd1b851a1860b3d5e57e4603b281
labanidas <labanidas2001@gmail.com>
2025-02-09 21:37:14 +0530

    quiz form

--- frontend/src/pages/Dashboard.vue
+++ frontend/src/pages/Dashboard.vue
@@ -1,16 +1,17 @@
 <script setup>
-import Sidebar from '@/components/Sidebar.vue'
+  import Sidebar from '@/components/Sidebar.vue'
+  import QuizFetchForm from '@/components/QuizFetchForm.vue'
 </script>
 
 <template>
-    <div class="flex min-h-screen bg-gray-100">
+
+  <div class="flex min-h-screen bg-gray-100">
     <!-- Sidebar -->
     <Sidebar />
 
     <!-- Main content area -->
     <div class="flex-1 p-8">
-      <h1 class="text-3xl font-bold text-gray-800">Dashboard</h1>
-      <p class="mt-4 text-lg text-gray-600">Welcome to the admin dashboard. You can manage your settings here.</p>
+      <QuizFetchForm />
 
       <!-- Additional content goes here -->
       <div class="mt-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
---
+++ frontend/src/store/useQuizStore.js
@@ -0,0 +1,35 @@
+// src/store/useQuizStore.js
+import { defineStore } from "pinia";
+import { ref } from "vue";
+import { axiosInstance } from "../lib/axios";
+import { useToast } from "vue-toast-notification";
+import { useRouter } from "vue-router";
+
+export const useQuizStore = defineStore('apiStore', () => {
+  const router = useRouter();
+  const toast = useToast();
+  const quiz_questions = ref([]); 
+  const isLoadingQuestions = ref(false);
+
+  // API call method using Axios
+  const fetchQuestions = async (data) => {
+    isLoadingQuestions.value = true;
+    try {
+      const response = await axiosInstance.post('/fetch-questions', data); 
+      console.log(response.data);     
+    } catch (error) {
+      toast.error(error.message || 'Something went wrong!', {
+        position: 'top-right',
+        timeout: 3000,
+      });
+    }finally{
+      isLoadingQuestions.value = false;
+    }
+  };
+
+  return {
+    quiz_questions,
+    isLoadingQuestions,
+    fetchQuestions,
+  };
+});
