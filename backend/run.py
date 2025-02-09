cd9875349cc7e6e87a46bb5dac8ec4f342e5c88c
labanidas <labanidas2001@gmail.com>
2025-02-09 14:11:14 +0530

    fix cors error

--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,10 @@
+from flask_cors import CORS  # Import CORS from flask_cors
 from app import create_app
 
 app = create_app()
+
+# Enable CORS for the app
+CORS(app)
 
 if __name__ == '__main__':
     app.run(debug=True)
