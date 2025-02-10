f1de06277dc36d02ce83c1f6ddf90d6f42a81436
labanidas <labanidas2001@gmail.com>
2025-02-11 00:47:41 +0530

    minor change

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,5 +3,5 @@ import axios from 'axios';
 
 export const axiosInstance = axios.create({
   baseURL: 'http://localhost:5000', 
-//   withCredentials: true, // Optional: If you are handling cookies or authentication
+  withCredentials: true, // Optional: If you are handling cookies or authentication
 });
